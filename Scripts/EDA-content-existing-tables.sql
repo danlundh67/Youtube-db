1d4ce5b31bd641bc480063deb71d897788ec4729
Dan Lund <dan.lundh@outlook.com>
2024-09-30 14:20:55 +0200

    EDA viewers age

--- Scripts/EDA-content-existing-tables.sql
+++ Scripts/EDA-content-existing-tables.sql
@@ -6,13 +6,25 @@ SELECT * FROM marts.device_summary;
 
 SELECT * FROM marts.device_views_date;
 
-SELECT * FROM geografi.tabelldata t ;
+SELECT * FROM marts.device_summary ds ;
+
+SELECT * FROM marts.views_per_date vpd ;
+
+SELECT * FROM geografi.totalt t ;
+
+SELECT * FROM innehall.tabelldata t ;
+
+
 
 SELECT * FROM stader.tabelldata d ;
 
 SELECT * FROM tittare.tabelldata_kon tk 
 
+SELECT * FROM tittare.tabelldata_alder ta ;
+
 SELECT * FROM prenumerationskalla.tabelldata t ;
 
 SELECT * FROM trafikkalla.tabelldata t ;
+
+SELECT * FROM operativsystem.tabelldata t ;
 
